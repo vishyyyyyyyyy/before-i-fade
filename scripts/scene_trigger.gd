a2c3ee2b55db33d849990121ab16d493c9b920b2
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-12-18 19:47:40 -0600

    bathroom collisions

--- scripts/scene_trigger.gd
+++ scripts/scene_trigger.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-@export var choose: String = "res://scenes/choosechar.tscn"
+@export var goBathroom: String = "res://scenes/bathroom.tscn"
 
 func _ready():
 	pass
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_file(choose)
+		get_tree().change_scene_to_file(goBathroom)
 		print("player entered")
