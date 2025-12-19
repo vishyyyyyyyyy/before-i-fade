a84fb46e5d650296560893d798ff0dbd33b9ba16
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-12-18 21:32:40 -0600

    reusable scene trigger script

--- scripts/bathroom.gd
+++ scripts/bathroom.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 func _ready():
-	#$SceneTrigger/CollisionShape2D.disabled =true
+	
+	$SceneTrigger/CollisionShape2D.disabled=true
 	$CanvasLayer3/AnimationPlayer.play("text")
 	await $CanvasLayer3/AnimationPlayer.animation_finished
-	#$CanvasLayer/Label3.visible=true
-	#$SceneTrigger/CollisionShape2D.disabled = false
+	$CanvasLayer3/Label3.visible=true
+	$SceneTrigger/CollisionShape2D.disabled = false
--- scripts/scene_trigger.gd
+++ scripts/scene_trigger.gd
@@ -1,11 +1,30 @@
 extends Area2D
 
-@export var goBathroom: String = "res://scenes/bathroom.tscn"
+@export var target_scene: String
+@export var requires_interaction: bool = false
+@export var interaction_action: String = "interact"
+
+var player_inside := false
 
 func _ready():
-	pass
+	input_pickable = true
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_file(goBathroom)
-		print("player entered")
+		if requires_interaction:
+			player_inside = true
+		else:
+			change_scene()
+
+func _on_body_exited(body):
+	if body is CharacterBody2D:
+		player_inside = false
+
+func _input_event(viewport, event, shape_idx):
+	if requires_interaction and event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			change_scene()
+			
+func change_scene():
+	if target_scene != "":
+		get_tree().change_scene_to_file(target_scene)
