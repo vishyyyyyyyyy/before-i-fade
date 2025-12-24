d14235ff2495669dbc0c613255992411d9ebb5c4
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-12-24 09:54:46 -0600

    living room challenge

--- scripts/livingroom.gd
+++ scripts/livingroom.gd
@@ -11,7 +11,12 @@ func _ready() -> void:
 	$CanvasLayer/CanvasModulate.color = Color(0.0, 0.992, 0.816)
 	$CanvasLayer4/CanvasModulate.color = Color(0.094, 0.323, 0.28) 
 	$CanvasLayer3/CanvasModulate.color = Color(0.0, 0.992, 0.816)
+	$ghostlayer/pianokeys.challengecompleted.connect(challengecompleted)
+	$ghostlayer/continue.pressed.connect(pressed)
 	text()
+func _process(delta: float) -> void:
+	time_left_seconds = $ghostlayer/Timer2.time_left
+	$ghostlayer/Label8.text = "%.1f" % time_left_seconds
 
 func text():
 	#if Global.character =="girlGhost":
@@ -112,8 +117,68 @@ func challenge():
 	$explore/CanvasLayer/flower/CollisionShape2D.disabled=true
 	$explore/CanvasLayer/piano/CollisionShape2D.disabled=true
 	$ghostlayer/Music.visible=true
+	$ghostlayer/Label.visible=true
+	$ghostlayer/Label2.visible=true
+	$ghostlayer/Label3.visible=true
+	$ghostlayer/Label4.visible=true
 	#if Global.character == "boyGhost":
 		#$ghostlayer/ghosttext3.play("boy")
 	#if Global.character == "girlGhost":
 		#$ghostlayer/ghosttext3.play("girl")
 	#await $ghostlayer/ghosttext3.animation_finished
+	$ghostlayer/ColorRect.visible=true
+	$ghostlayer/Menucard.visible=true
+	$ghostlayer/Label5.visible=true
+	$ghostlayer/Label6.visible=true
+	$ghostlayer/Label7.visible=true
+	$ghostlayer/continue.visible=true
+	$ghostlayer/continue/CollisionShape2D.disabled=false
+
+func pressed():
+	$ghostlayer/Label9.visible=true
+	$ghostlayer/Label.visible=true
+	$ghostlayer/Label2.visible=true
+	$ghostlayer/Label3.visible=true
+	$ghostlayer/Label4.visible=true
+	$ghostlayer/Label8.visible=true
+	$ghostlayer/ColorRect.visible=false
+	$ghostlayer/Menucard.visible=false
+	$ghostlayer/Label5.visible=false
+	$ghostlayer/Label6.visible=false
+	$ghostlayer/Label7.visible=false
+	$ghostlayer/continue.visible=false
+	$ghostlayer/Timer.visible=true
+	$ghostlayer/Timer2.start()
+	$ghostlayer/pianokeys/CollisionShape2D.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D2.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D3.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D4.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D5.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D6.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D7.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D8.disabled=false
+	$ghostlayer/pianokeys/CollisionShape2D9.disabled=false
+	
+func challengecompleted():
+	$explore/CanvasLayer/bookshelf/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/clock/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/couch/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/table/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/tv/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/rug/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/flower/CollisionShape2D.disabled=true
+	$explore/CanvasLayer/piano/CollisionShape2D.disabled=true
+	$ghostlayer/Correct.visible=false
+	$ghostlayer/ColorRect.visible=true
+	$ghostlayer/Diarypage.visible=true
+	$ghostlayer/Label10.visible=true
+	$ghostlayer/Label11.visible=true
+	$ghostlayer/Label12.visible=true
+	await get_tree().create_timer(5).timeout
+	$ghostlayer/ColorRect.visible=false
+	$ghostlayer/Diarypage.visible=false
+	$ghostlayer/Label10.visible=false
+	$ghostlayer/Label11.visible=false
+	$ghostlayer/Label12.visible=false
+	
+	
--- scripts/piano.gd
+++ scripts/piano.gd
@@ -1,41 +1,126 @@
 extends Area2D
+signal challengecompleted
+@onready var shapes = [
+	[$CollisionShape2D,  "triangle", $CollisionShape2D/AudioStreamPlayer],
+	[$CollisionShape2D2, "star",     $CollisionShape2D2/AudioStreamPlayer],
+	[$CollisionShape2D3, "circle",   $CollisionShape2D3/AudioStreamPlayer],
+	[$CollisionShape2D4, "arrow",    $CollisionShape2D4/AudioStreamPlayer],
+	[$CollisionShape2D5, "x",        $CollisionShape2D5/AudioStreamPlayer],
+	[$CollisionShape2D6, "diamond",  $CollisionShape2D6/AudioStreamPlayer],
+	[$CollisionShape2D7, "cat",      $CollisionShape2D7/AudioStreamPlayer],
+	[$CollisionShape2D8, "check",    $CollisionShape2D8/AudioStreamPlayer],
+	[$CollisionShape2D9, "h",        $CollisionShape2D9/AudioStreamPlayer]
+]
+var solutions = [
+	["triangle", "cat", "circle", "x"],
+	["star", "arrow", "diamond", "check"],
+	["star", "triangle", "arrow", "h", "cat", "circle", "diamond", "diamond"]
+]
 
+var current_solution := 0
+var selected := []
+var input_locked := false
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree
 func _ready() -> void:
-	pass # Replace with function body.
-
-var solution = {"triangle", "cat", "circle", "x"}
+	start_solution()
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton \
-	and event.pressed \
-	and event.button_index == MOUSE_BUTTON_LEFT:
+	if input_locked:
+		return
 
-			if shape_idx == 0:
-				$CollisionShape2D/ColorRect.visible=true
-				print("triangle")
-			if shape_idx == 1:
-				$CollisionShape2D2/ColorRect.visible=true
-				print("star")
-			if shape_idx == 2:
-				$CollisionShape2D3/ColorRect.visible=true
-				print("circle")
-			if shape_idx == 3:
-				$CollisionShape2D4/ColorRect.visible=true
-				print("arrow")
-			if shape_idx == 4:
-				$CollisionShape2D5/ColorRect.visible=true
-				print("x")
-			if shape_idx == 5:
-				$CollisionShape2D6/ColorRect.visible=true
-				print("diamond")
-			if shape_idx == 6:
-				$CollisionShape2D7/ColorRect.visible=true
-				print("cat")
-			if shape_idx == 7:
-				$CollisionShape2D8/ColorRect.visible=true
-				print("check")
-			if shape_idx == 8:
-				$CollisionShape2D9/ColorRect.visible=true
-				print("h")
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		hidepianotile()
+
+		var shape_node = shapes[shape_idx][0]
+		var shape_name = shapes[shape_idx][1]
+		var audio_player = shapes[shape_idx][2]
+
+		shape_node.get_node("ColorRect").visible = true
+		audio_player.play()
+		selected.append(shape_name)
+
+		if selected.size() == solutions[current_solution].size():
+			check_solution()
+
+func check_solution():
+	input_locked = true
+
+	if selected == solutions[current_solution]:
+		print("✅ SOLUTION ", current_solution + 1, " CORRECT")
+
+		current_solution += 1
+
+	
+		if current_solution == 1:
+			$"../Music2".visible = true
+			start_solution()
+
+		elif current_solution == 2:
+			$"../Music3".visible = true
+			start_solution()
+			
+		elif current_solution == 3:
+			
+			print("🎉 ALL SOLUTIONS COMPLETE")
+			$"../Correct".visible=true
+			$"../AudioStreamPlayer".play()
+			await get_tree().create_timer(2).timeout
+			$"../Correct".visible=false
+			disablecollisionshape()
+			emit_signal("challengecompleted")
+			return
+	else:
+		$"../Wrong".visible=true
+		$"../AudioStreamPlayer2".play()
+		await get_tree().create_timer(2).timeout
+		$"../Wrong".visible=false
+		start_solution()
+		
+
+	input_locked = false
+
+
+
+func start_solution():
+	selected.clear()
+	hidepianotile()
+
+func hidepianotile():
+	$CollisionShape2D/ColorRect.visible = false
+	$CollisionShape2D2/ColorRect.visible = false
+	$CollisionShape2D3/ColorRect.visible = false
+	$CollisionShape2D4/ColorRect.visible = false
+	$CollisionShape2D5/ColorRect.visible = false
+	$CollisionShape2D6/ColorRect.visible = false
+	$CollisionShape2D7/ColorRect.visible = false
+	$CollisionShape2D8/ColorRect.visible = false
+	$CollisionShape2D9/ColorRect.visible = false
+
+func disablecollisionshape():
+	$CollisionShape2D.disabled=true
+	$CollisionShape2D2.disabled=true
+	$CollisionShape2D3.disabled=true
+	$CollisionShape2D4.disabled=true
+	$CollisionShape2D5.disabled=true
+	$CollisionShape2D6.disabled=true
+	$CollisionShape2D7.disabled=true
+	$CollisionShape2D8.disabled=true
+	$CollisionShape2D9.disabled=true
+
+func reset_to_beginning():
+	current_solution = 0
+	selected.clear()
+	input_locked = false
+	hidepianotile()
+	$"../Timer2".start()
+	$"../Music2".visible = false
+	$"../Music3".visible = false
+
+
+func _on_timer_2_timeout() -> void:
+	$"../Wrong".visible = true
+	$"../AudioStreamPlayer2".play()
+	await get_tree().create_timer(2).timeout
+	$"../Wrong".visible = false
+	reset_to_beginning()
