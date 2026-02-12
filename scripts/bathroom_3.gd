1aac8bd7ac46dd90fb2584aebb7c422885445428
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-02-12 15:26:49 -0600

    bathroom 3 text skip !

--- scripts/bathroom_3.gd
+++ scripts/bathroom_3.gd
@@ -1,5 +1,27 @@
 extends Node2D
 
+var segment_data := [
+	{ "starts": [0.0], "ends": [2.0] }, #ghost enter bathroom
+	{ "starts": [0.0, 4.0, 8.0, 12.0], "ends": [2.0, 6.0, 10.0, 14.0] }, #friend phone call
+	{ "starts": [0.0, 4.0, 8.0, 12.0, 20.0], "ends": [2.0, 6.0, 10.0, 16.0, 24.0] }, #ghost say friend annoying
+	{ "starts": [0.0, 4.0, 8.0], "ends": [2.0, 6.0, 10.0] }, #find comb
+]
+@onready var anim_players := [
+	$CanvasLayer3/ghosttalk, #ghost enter bathroom
+	$CanvasLayer3/friendphone ,
+	$CanvasLayer3/ghosttalk2,
+	$CanvasLayer3/ghosttalk3 #find comb
+]
+var anim_index := 0
+var anim: AnimationPlayer
+var dialogue_active := false
+var segment_index := 0
+var animating := true
+var segment_starts
+var segment_ends
+	
+signal dialogue_finished(index)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,9 +34,8 @@ func _ready() -> void:
 		$CanvasLayer3/AnimationPlayer/boy.visible = false
 	Global.reusabledesk =1
 	print(Global.reusabledesk)
-	firsttext()
-	await $CanvasLayer3/ghosttalk.animation_finished
-	await get_tree().create_timer(0.5).timeout
+	await start_dialogue(0)
+	await get_tree().create_timer(1).timeout
 	if (Global.character == "girlGhost"):
 		$CanvasLayer3/AnimationPlayer/girl.visible = true
 	if (Global.character == "boyGhost"):
@@ -47,17 +68,15 @@ func _ready() -> void:
 	$CanvasLayer3/AnimationPlayer2/Phone.visible=true
 	await $AudioStreamPlayer.finished
 	$CanvasLayer3/AnimationPlayer2/Phone.visible=false
-	friendCall()
-	await get_tree().create_timer(16).timeout
+	await start_dialogue(1)
 	if (Global.character == "girlGhost"):
 		$CanvasLayer3/AnimationPlayer.play("girlExit")
 		await $CanvasLayer3/AnimationPlayer.animation_finished
 	if (Global.character == "boyGhost"):
 		$CanvasLayer3/AnimationPlayer.play("boyExit")
 		await $CanvasLayer3/AnimationPlayer.animation_finished
-	secondtext()
-	await $CanvasLayer3/ghosttalk2.animation_finished
-	$CanvasLayer3/AnimationPlayer3
+	await start_dialogue(2)
+	$CanvasLayer3/ghosttalk2/skip.visible=false
 	$CanvasLayer3/Node3/ColorRect.visible=true
 	$CanvasLayer3/Node3/Menucard.visible=true
 	$CanvasLayer3/Node3/Label2.visible=true
@@ -68,12 +87,181 @@ func _ready() -> void:
 	$CanvasLayer3/Node3/continue.pressed.connect(_on_button_pressed)
 	$CanvasLayer3/PresentTile.challengecompleted.connect(afterpuzzle)
 	$CanvasLayer3/comb.combpress.connect(combpress)
-func firsttext():
-	if Global.character=="girlGhost":
-		$CanvasLayer3/ghosttalk.play("girltext")
-	if Global.character=="boyGhost":
-		$CanvasLayer3/ghosttalk.play("boytext")
 	
+func _process(_delta):
+	if not dialogue_active or not animating:
+		return
+	
+	if anim.current_animation == "":
+		return # No animation playing, skip
+
+	if anim.get_current_animation_position() >= segment_ends[segment_index]:
+		anim.pause()
+		animating = false
+
+func start_dialogue(index: int):
+	# Safety
+	if index >= anim_players.size():
+		return
+
+	# Stop all animations
+	for a in anim_players:
+		a.stop()
+
+	anim_index = index
+	anim = anim_players[anim_index]
+
+	segment_starts = segment_data[anim_index].starts
+	segment_ends   = segment_data[anim_index].ends
+
+	segment_index = 0
+	animating = true
+	dialogue_active = true
+	
+	var anim_name := ""
+	
+	if anim_index == 0:
+		
+		if Global.character =="girlGhost":
+			anim_name = "girltext"
+
+		elif Global.character =="boyGhost":
+			anim_name ="boytext"
+		else:
+			print("error animating text")
+	
+	if anim_index == 1:
+		$CanvasLayer3/ghosttalk/skip.visible=false
+		$CanvasLayer3/ghosttalk/Label.visible=false
+		$CanvasLayer3/ghosttalk/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk/GirlGhost.visible=false
+		if Global.character =="girlGhost":
+#basically hide stuff from prev anim that didn’t finish, like last segment here
+			anim_name = "friendcallgirl"
+
+		elif Global.character =="boyGhost":
+			anim_name ="friendcallboy"
+		else:
+			print("error animating text")
+			
+	if anim_index == 2:
+		$CanvasLayer3/ghosttalk/skip.visible=false
+		$CanvasLayer3/ghosttalk/Label.visible=false
+		$CanvasLayer3/ghosttalk/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk/GirlGhost.visible=false
+		$"CanvasLayer3/friendphone/Friend label".visible=false
+		$CanvasLayer3/friendphone/Label4.visible=false
+		$CanvasLayer3/friendphone/skip.visible=false
+		$CanvasLayer3/friendphone/Friend2.visible=false
+		$CanvasLayer3/friendphone/Girl2.visible=false
+		$CanvasLayer3/friendphone/YNlabel.visible=false
+		$CanvasLayer3/friendphone/Friend.visible=false
+		$CanvasLayer3/friendphone/Boy2.visible=false
+		if Global.character =="girlGhost":
+			anim_name = "girltext2"
+
+		elif Global.character =="boyGhost":
+			anim_name ="boytext2"
+		else:
+			print("error animating text")
+			
+	if anim_index == 3:
+		$CanvasLayer3/ghosttalk/skip.visible=false
+		$CanvasLayer3/ghosttalk/Label.visible=false
+		$CanvasLayer3/ghosttalk/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk/GirlGhost.visible=false
+		$"CanvasLayer3/friendphone/Friend label".visible=false
+		$CanvasLayer3/friendphone/Label4.visible=false
+		$CanvasLayer3/friendphone/skip.visible=false
+		$CanvasLayer3/friendphone/Friend2.visible=false
+		$CanvasLayer3/friendphone/Girl2.visible=false
+		$CanvasLayer3/friendphone/YNlabel.visible=false
+		$CanvasLayer3/friendphone/Friend.visible=false
+		$CanvasLayer3/friendphone/Boy2.visible=false
+		
+		$CanvasLayer3/ghosttalk2/Label5.visible=false
+		$CanvasLayer3/ghosttalk2/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk2/GirlGhost.visible=false
+		$CanvasLayer3/ghosttalk2/GirlGhost2.visible=false
+		$CanvasLayer3/ghosttalk2/skip.visible=false
+		
+		if Global.character =="girlGhost":
+			anim_name = "girlghost"
+
+		elif Global.character =="boyGhost":
+			anim_name ="boyghost"
+		else:
+			print("error animating text")
+		
+		
+	anim.play(anim_name) 
+	   
+	await dialogue_finished
+
+func _input(event):
+	if not dialogue_active:
+		return
+
+	if event.is_action_pressed("ui_accept") and not event.is_echo():
+		textskip()	
+
+func end_dialogue():
+	dialogue_active = false
+	anim.stop()
+
+	print("Dialogue finished:", anim_index)
+	if anim_index == 0:
+		$CanvasLayer3/ghosttalk/skip.visible=false
+		$CanvasLayer3/ghosttalk/Label.visible=false
+		$CanvasLayer3/ghosttalk/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk/GirlGhost.visible=false
+		
+			
+	if anim_index ==1:
+		$"CanvasLayer3/friendphone/Friend label".visible=false
+		$CanvasLayer3/friendphone/Label4.visible=false
+		$CanvasLayer3/friendphone/skip.visible=false
+		$CanvasLayer3/friendphone/Friend2.visible=false
+		$CanvasLayer3/friendphone/Girl2.visible=false
+		$CanvasLayer3/friendphone/YNlabel.visible=false
+		$CanvasLayer3/friendphone/Friend.visible=false
+		$CanvasLayer3/friendphone/Boy2.visible=false
+	
+	if anim_index == 2:
+		$CanvasLayer3/ghosttalk2/Label5.visible=false
+		$CanvasLayer3/ghosttalk2/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk2/GirlGhost.visible=false
+		$CanvasLayer3/ghosttalk2/GirlGhost2.visible=false
+		$CanvasLayer3/ghosttalk2/skip.visible=false
+	
+	if anim_index ==3: 
+		$CanvasLayer3/ghosttalk3/skip.visible=false
+		$CanvasLayer3/ghosttalk3/Label3.visible=false
+		$CanvasLayer3/ghosttalk3/GirlGhost.visible=false
+		$CanvasLayer3/ghosttalk3/BoyGhost.visible=false
+		$CanvasLayer3/ghosttalk3/GirlGhost2.visible=false
+		$CanvasLayer3/ghosttalk3/BoyGhost2.visible=false
+		
+		
+	emit_signal("dialogue_finished", anim_index)
+
+func textskip():
+	if animating:
+		anim.seek(segment_ends[segment_index], true)
+		anim.pause()
+		animating = false
+	else:
+		segment_index += 1 
+
+		if segment_index < segment_starts.size():
+			anim.seek(segment_starts[segment_index], true)
+			anim.play()
+			animating = true
+		else:
+			print("call end dialogue")
+			end_dialogue()
+
+
 func friendCall():
 	if Global.character=="girlGhost":
 		$CanvasLayer3/friendphone.play("friendcallgirl")
@@ -81,13 +269,6 @@ func friendCall():
 	if Global.character=="boyGhost":
 		$CanvasLayer3/friendphone.play("friendcallboy")
 		await $CanvasLayer3/friendphone.animation_finished
-func secondtext():
-	if Global.character=="girlGhost":
-		$CanvasLayer3/ghosttalk2.play("girltext2")
-		await 	$CanvasLayer3/ghosttalk2.animation_finished
-	if Global.character=="boyGhost":
-		$CanvasLayer3/ghosttalk2.play("boytext2")
-		await 	$CanvasLayer3/ghosttalk2.animation_finished
 		
 func _on_button_pressed():
 	print("Signal received in main script!")
@@ -219,22 +400,12 @@ func combpress():
 	$CanvasLayer3/comb.visible=false
 	$CanvasLayer3/Comb2.visible=true
 	$CanvasLayer3/comb/CollisionShape2D.disabled=true
-	if Global.character=="girlGhost":
-		$CanvasLayer3/ghosttalk3.play("girlghost")
-		$CanvasLayer3/blobGhostPlayer.position.x=1800
-		await $"CanvasLayer3/ghosttalk3".animation_finished
-		$CanvasLayer3/TileMap3.visible=false
-		$CanvasLayer3/comb.visible=true
-		$CanvasLayer3/Comb2.visible=false
-		$CanvasLayer3/Label7.visible=true
-	if Global.character=="boyGhost":
-		$CanvasLayer3/ghosttalk3.play("boyghost")
-		$CanvasLayer3/blobGhostPlayer.position.x=1800
-		await $"CanvasLayer3/ghosttalk3".animation_finished
-		$CanvasLayer3/TileMap3.visible=false
-		$CanvasLayer3/comb.visible=true
-		$CanvasLayer3/Comb2.visible=false
-		$CanvasLayer3/Label7.visible=true
+	$CanvasLayer3/blobGhostPlayer.position.x=1800
+	await start_dialogue(3)
+	$CanvasLayer3/TileMap3.visible=false
+	$CanvasLayer3/comb.visible=true
+	$CanvasLayer3/Comb2.visible=false
+	$CanvasLayer3/Label7.visible=true
 		
 func modulatebackghost():
 	$CanvasLayer/TileMap.visible=false
