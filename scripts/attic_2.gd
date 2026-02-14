3bd4a9d7d20ff71e7e15720eeb1eab450de60fae
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-02-14 14:15:47 -0600

    attic 2 skip text !

--- scripts/attic_2.gd
+++ scripts/attic_2.gd
@@ -1,5 +1,105 @@
 extends Node2D
 
+var segment_data := [
+	{ "starts": [0.0, 4.0], "ends": [2.0, 6.0] }, 	
+]
+@onready var anim_players := [
+	$ghostlayer/ghosttext
+]
+var anim_index := 0
+var anim: AnimationPlayer
+var dialogue_active := false
+var segment_index := 0
+var animating := true
+var segment_starts
+var segment_ends
+	
+signal dialogue_finished(index)
+
+func _process(_delta):
+	if not dialogue_active or not animating:
+		return
+	
+	if anim.current_animation == "":
+		return # No animation playing, skip
+
+	if anim.get_current_animation_position() >= segment_ends[segment_index]:
+		anim.pause()
+		animating = false
+
+func start_dialogue(index: int):
+	# Safety
+	if index >= anim_players.size():
+		return
+
+	# Stop all animations
+	for a in anim_players:
+		a.stop()
+
+	anim_index = index
+	anim = anim_players[anim_index]
+
+	segment_starts = segment_data[anim_index].starts
+	segment_ends   = segment_data[anim_index].ends
+
+	segment_index = 0
+	animating = true
+	dialogue_active = true
+	
+	var anim_name := ""
+	
+	if anim_index == 0:
+		if Global.character =="girlGhost":
+			anim_name = "girl"
+
+		elif Global.character =="boyGhost":
+			anim_name ="boy"
+		else:
+			print("error animating text")
+			
+	anim.play(anim_name) 
+	   
+	await dialogue_finished 
+
+
+
+func _input(event):
+	if not dialogue_active:
+		return
+
+	if event.is_action_pressed("ui_accept") and not event.is_echo():
+		textskip()	
+
+
+func end_dialogue():
+	dialogue_active = false
+	anim.stop()
+
+	print("Dialogue finished:", anim_index)
+	if anim_index == 0:
+		$ghostlayer/ghosttext/Label2.visible=false
+		$ghostlayer/ghosttext/GirlGhost.visible=false
+		$ghostlayer/ghosttext/BoyGhost.visible=false
+		$ghostlayer/ghosttext/skip.visible=false
+		
+	emit_signal("dialogue_finished", anim_index)
+
+
+func textskip():
+	if animating:
+		anim.seek(segment_ends[segment_index], true)
+		anim.pause()
+		animating = false
+	else:
+		segment_index += 1 
+
+		if segment_index < segment_starts.size():
+			anim.seek(segment_starts[segment_index], true)
+			anim.play()
+			animating = true
+		else:
+			print("call end dialogue")
+			end_dialogue()
 
 func _ready() -> void:
 	Global.ending= 2
@@ -24,8 +124,7 @@ func _ready() -> void:
 		$ghostlayer/blobGhostPlayer.position.x=1205
 		$ghostlayer/blobGhostPlayer.position.y=641
 		await $ghostlayer/AnimationPlayer.animation_finished
-		$ghostlayer/ghosttext.play("girl")
-		await $ghostlayer/ghosttext.animation_finished
+		await start_dialogue(0)
 		await get_tree().create_timer(0.3).timeout
 		await fade_out_node($ghostlayer/blobGhostPlayer, 2.5)
 		get_tree().change_scene_to_file("res://scenes/endcreds.tscn")
@@ -43,8 +142,7 @@ func _ready() -> void:
 		$ghostlayer/blobGhostPlayer.position.x=1205
 		$ghostlayer/blobGhostPlayer.position.y=641
 		await $ghostlayer/AnimationPlayer.animation_finished
-		$ghostlayer/ghosttext.play("boy")
-		await $ghostlayer/ghosttext.animation_finished
+		await start_dialogue(0)
 		await get_tree().create_timer(0.3).timeout
 		await fade_out_node($ghostlayer/blobGhostPlayer, 2.5)
 		get_tree().change_scene_to_file("res://scenes/endcreds.tscn")
