3579bdba5b76c13e70c28c98d2a1882145e2d574
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-03-11 21:45:01 -0500

    pause menu all bedroom diary entry

--- scripts/bedroomdiaryentry_3.gd
+++ scripts/bedroomdiaryentry_3.gd
@@ -1,10 +1,27 @@
 extends Node2D
 
+@onready var pause_menu = $CanvasPause/PauseMenu
+func toggle_pause():
+	$CanvasPause/PauseMenu/resume/Label.text = "Game Paused"
+	get_tree().paused = !get_tree().paused
+	pause_menu.visible = get_tree().paused
+	$CanvasPause/ColorRect2.visible=true
+	$CanvasPause/Menucard2.visible=true
+
 
 func _ready() -> void:
+	if MusicManager.music_on:
+		$CanvasPause/PauseMenu/music/Label.text = "Music: ON"
+	else:
+		$CanvasPause/PauseMenu/music/Label.text = "Music: OFF"
+		
 	$CanvasLayer5/Label.visible=true
 	$desk.diaryentry5.connect(diaryentry5)
-	
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		toggle_pause()
+		
 func diaryentry5():
 	if Global.character=="boyGhost":
 		$CanvasLayer4/diaryentry5.play("boy")
@@ -27,3 +44,49 @@ func diaryentry5():
 		$desk/CollisionPolygon2D.disabled=true
 		$CanvasLayer4/Area2D/CollisionShape2D.disabled=false
 		$SceneTrigger/CollisionShape2D.disabled=true
+
+func _on_resume_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_tree().paused = false
+			$CanvasPause/PauseMenu.visible = false
+			$CanvasPause/PauseMenu/resume/Label.text = "Game Resumed"
+			$CanvasPause/ColorRect2.visible=false
+			$CanvasPause/Menucard2.visible=false
+			print("pressed") 
+
+
+func _on_music_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		MusicManager.toggle_music()
+
+		if MusicManager.music_on:
+			$CanvasPause/PauseMenu/music/Label.text = "Music: ON"
+		else:
+			$CanvasPause/PauseMenu/music/Label.text = "Music: OFF"
+
+
+func _on_controls_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		$CanvasPause/settingsControl.visible=true
+		$CanvasPause/PauseMenu.visible=false
+
+
+func _on_main_menu_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		get_tree().paused = false
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	
+ 
+
+func _on_close_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		get_tree().paused = !get_tree().paused
+		pause_menu.visible = get_tree().paused
+		$CanvasPause/PauseMenu.visible=false
+		$CanvasPause/ColorRect2.visible=false
+		$CanvasPause/Menucard2.visible=false
+
+
+func _on_settingsclose_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	$CanvasPause/settingsControl.visible=false
+	$CanvasPause/PauseMenu.visible=true
